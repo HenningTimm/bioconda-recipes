7d43e0f28463961a30e63d373b116896f8a6be3d
Henning Timm <henning.timm@tu-dortmund.de>
2017-07-11 14:29:17 +0200

    minor

--- recipes/taxmapper/build.sh
+++ recipes/taxmapper/build.sh
@@ -3,7 +3,7 @@
 if [ $(uname) == Linux ] ; then
     ## libstdc++ is expected in lib64, so link it there
     if [ ! -d $PREFIX/lib64 ] ; then mkdir $PREFIX/lib64 ; fi
-    ln -s $PREFIX/lib/libstdc++.la $PREFIX/lib64/libstdc++.la 
+    # ln -s $PREFIX/lib/libstdc++.la $PREFIX/lib64/libstdc++.la 
     ln -s $PREFIX/lib/libstdc++.so $PREFIX/lib64/libstdc++.so 
 fi
 
