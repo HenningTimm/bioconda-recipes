ee22365d1a4753caf1b6ed99384271559b4e59a3
Henning Timm <henning.timm@tu-dortmund.de>
2017-07-11 14:16:15 +0200

    Linking libstdc++ (copied from locarna recipe)

--- recipes/taxmapper/build.sh
+++ recipes/taxmapper/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+if [ $(uname) == Linux ] ; then
+    ## libstdc++ is expected in lib64, so link it there
+    if [ ! -d $PREFIX/lib64 ] ; then mkdir $PREFIX/lib64 ; fi
+    ln -s $PREFIX/lib/libstdc++.la $PREFIX/lib64/libstdc++.la 
+    ln -s $PREFIX/lib/libstdc++.so $PREFIX/lib64/libstdc++.so 
+fi
+
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
